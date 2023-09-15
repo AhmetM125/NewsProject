a143c2503909ea4fd5e3a334b95d5d3f31d7673a
Ahmet Yurdal <Ahmet_Yurdal_2000@hotmail.com>
2023-09-15 13:55:54 +0300

    Add project files.

---
+++ DataAccessLayer/Context/NEUContext.cs
@@ -0,0 +1,28 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Context
+{
+	public class NEUContext : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("server=LPTNET052\\SQLEXPRESS;database=M_NewsDb;integrated security=true;Encrypt=false");
+		}
+		/*public Context(DbContextOptions<Context> options) : base(options)
+		{
+
+		}
+*/
+		public DbSet<Admin> Admins { get; set; }
+		public DbSet<New> News { get; set; }
+
+
+	}
+}
---
+++ EntityLayer/Concrete/Admin.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EntityLayer.Concrete
+{
+	public class Admin
+	{
+		[Key]
+		public Guid User_Guid_Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
